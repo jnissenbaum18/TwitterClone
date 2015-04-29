61d1d53c1c962bfd49c062eed6b4adba1c4c3c3d
jnissenbaum18 <jnissenbaum@verizon.net>
2015-04-29 14:15:49 -0400

    Updated son

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
 	res.send('hello, world!')
 })
 
+app.get('/news', function (req, res){
+	res.send("You're reading news!")
+})
+
 var server = app.listen(3000, function () {
 
 	var host = server.address().address;
