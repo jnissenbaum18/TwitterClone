d430a21a4682903b2b760beb0c78fe05ed31fabe
Rachel Charow <rachel.charow@gmail.com>
2015-04-29 19:07:50 -0400

    Fix name bug

--- routes/index.js
+++ routes/index.js
@@ -3,31 +3,34 @@ var router = express.Router();
 // could use one line instead: var router = require('express').Router();
 var tweetBank = require('../tweetBank');
 
-router.get('/', function (req, res) {
-  var tweets = tweetBank.list();
-  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
-});
+module.exports = function(io){
+	router.get('/', function (req, res) {
+	  var tweets = tweetBank.list();
+	  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );
+	});
 
-router.get('/users/:name', function(req, res) {
-  var name = req.params.name;
-  var list = tweetBank.find( {name: name} );
-  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm: true } );
-});
+	router.get('/users/:name', function(req, res) {
+	  var name = req.params.name;
+	  var list = tweetBank.find( {name: name} );
+	  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list, showForm: true } );
+	});
 
-router.get('/tweets/:id', function(req, res) {
-  
-  var id = req.params.id;
-  var list = tweetBank.find( {id: +id} );
-  var name = list[0].name;
-  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
-});
+	router.get('/tweets/:id', function(req, res) {
+	  
+	  var id = req.params.id;
+	  var list = tweetBank.find( {id: +id} );
+	  var name = list[0].name;
+	  res.render( 'index', { title: 'Twitter.js - Posts by '+name, tweets: list } );
+	});
 
-router.post('/submit', function(req, res) {
-  var name = req.body.name;
-  var text = req.body.text;
-  tweetBank.add(name, text);
-  res.redirect('/');
-});
+	router.post('/submit', function(req, res) {
+	  var name = req.body.name;
+	  var text = req.body.text;
+	  tweetBank.add(name, text);
+	  io.sockets.emit("new_tweet",{ name: name, text: text });
+	  res.redirect('/');
+	});
 
+	return router;
+}
 
-module.exports = router;
